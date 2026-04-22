6a3cd32e778b2f3b61c1510d2a8d230bde1b43fc
Priyanshi Verma <priyanshi.25bai11198@vitbhopal.ac.in>
2026-04-22 19:47:32 +0530

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,5 +1,3 @@
-Date,Amount,Category
-2026-03-31,1500,Food
-2026-03-31,500,Transport
-2026-04-01,2000,Study
-2026-04-01,1000,Entertainment
+200|Food|Burger|2026-03-20
+500|Travel|Uber ride|2026-03-21
+300|Shopping|T-shirt|2026-03-22
